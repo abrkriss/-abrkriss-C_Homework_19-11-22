ff8aea9a234d03a37812f5b6bca36e82b575caba
Kristina <abrkriss@gmail.com>
2022-11-24 22:42:14 +0300

    Zadacha_1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,24 @@
+﻿void Zadacha19()
+{
+    //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+    Console.WriteLine("Введите пятизначное число: ");
+    string num = Console.ReadLine();
+
+   
+    if ((num[0] == num[4] || num[1] == num[3]) && (num!.Length == 5))
+    {
+        Console.WriteLine(num + " - является палиндромом");
+    }
+    else if   (num!.Length != 5)
+    {
+        Console.WriteLine("Введено некорректное число. ");
+    }
+    else
+    {
+         Console.WriteLine(num + " -  не является палиндромом");
+    }
+
+   
+}
+Zadacha19();
