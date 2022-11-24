de8e2302d142f55c6189b93bfba2804ace49e656
Kristina <abrkriss@gmail.com>
2022-11-24 23:24:47 +0300

    Zadacha_3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,13 @@
+﻿void Zadacha23()
+{
+    //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+    Console.WriteLine("Введите число максимального значения в таблице квадратов: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    
+            for(int i = 1; i <= N; i++)
+                Console.WriteLine(i + "^3" + "\t" + "=" + "\t" + i*i*i);
+
+}
+Zadacha23();
